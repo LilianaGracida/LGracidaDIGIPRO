fee88014988f2c187dd766847f205c5fa80adfee
LilianaGracida <lgbrioso@gmail.com>
2023-05-16 17:19:25 -0600

    Add project files.

---
+++ DL/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class Conexion
+    {
+        public static string GetConnectionString()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.ConnectionStrings["LGracidaDIGIPRO"].ConnectionString;
+
+            return cadenaConexion;
+        }
+    }
+}
